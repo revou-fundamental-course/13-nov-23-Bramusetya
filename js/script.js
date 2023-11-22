08763286f576eef62110e2144517e45504771ac2
Falih M Farid B <muhamuhammadfalih018@gmail.com>
2023-11-22 22:28:53 +0700

    modify javascript

--- js/script.js
+++ js/script.js
@@ -5,14 +5,6 @@ function replaceName(){
 
 replaceName();
 
-// function scrollToSection(sectionId) {
-//     var section = document.getElementById(sectionId);
-
-//     if (section) {
-//         section.scrollIntoView({ behavior: 'smooth' });
-//     }
-// }
-
 
 function validateForm() {
     var name = document.getElementById('name').value;
